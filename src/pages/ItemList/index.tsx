a0f326fba9a54aab77dd4c8c51b0daaf799d6aa6
Oliver Roick <oliver@roick.email>
2023-12-01 15:29:52 +1100

    feat: Add pagination handle

--- src/pages/ItemList/index.tsx
+++ src/pages/ItemList/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Flex,
+import {
+  Flex,
   Heading,
   TableContainer,
   Table,
@@ -10,6 +11,8 @@ import { Flex,
   Td,
   Tbody,
   Button,
+  Select,
+  Text,
   usePrevious
 } from "@chakra-ui/react";
 import { useStacSearch } from "@developmentseed/stac-react";
@@ -27,12 +30,15 @@ function ItemList() {
     state,
     sortby,
     setSortby,
+    limit,
+    setLimit,
     submit,
     nextPage,
     previousPage,
     ...searchState
   } = useStacSearch();
   const previousSortby = usePrevious(sortby);
+  const previousLimit = usePrevious(limit);
 
   useEffect(() => {
     if (results) return;
@@ -40,11 +46,11 @@ function ItemList() {
   }, [submit]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
-    // Automatically execute a new item search if the sorting has changed
-    if (sortby !== previousSortby) {
+    // Automatically execute a new item search if the sorting or limit have changed
+    if (sortby !== previousSortby || limit !== previousLimit) {
       submit();
     }
-  }, [sortby, previousSortby, submit]);
+  }, [sortby, previousSortby, submit, limit, previousLimit]);
 
   const sort = sortby?.length ? sortby[0] : undefined;
   const handleSort = (sort: Sort) => setSortby([ sort ]);
@@ -79,7 +85,22 @@ function ItemList() {
               </Tbody>
             </Table>
           </TableContainer>
-          <Flex mt="4" display="flex" justifyContent="flex-end" gap="2" fontSize="sm">
+          <Flex mt="4" display="flex" gap="2" fontSize="sm" alignItems="baseline">
+            <Flex flex="1" gap="2" alignItems="baseline">
+              <Select
+                value={limit}
+                onChange={(e) => setLimit(parseInt(e.target.value))}
+                w="20"
+                size="sm"
+                id="limit"
+              >
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+              </Select>
+              <Text as="label" htmlFor="limit">items per page</Text>
+            </Flex>
             { previousPage && <Button variant="link" size="sm" onClick={previousPage}>Previous page</Button>}
             { (previousPage && nextPage) && " | "}
             { nextPage && <Button variant="link" size="sm" onClick={nextPage}>Next page</Button> }
